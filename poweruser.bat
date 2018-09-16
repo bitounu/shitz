e4a55d337e0f47b3ca26c6e301e9882d647a8740
paczor <38018844+bitounu@users.noreply.github.com>
2018-09-16 20:13:17 +0200

    Dodany skrypt dla PowerUsera w Windows

---
+++ poweruser.bat
@@ -0,0 +1,15 @@
+ntrights.exe -U "Power Users" +R SeInteractiveLogonRight
+ntrights.exe -U "Power Users" +R SeChangeNotifyPrivilege
+ntrights.exe -U "Power Users" +R SeSystemtimePrivilege
+ntrights.exe -U "Power Users" +R SeTimeZonePrivilege
+ntrights.exe -U "Power Users" +R SeCreatePagefilePrivilege
+ntrights.exe -U "Power Users" +R SeCreateGlobalPrivilege
+ntrights.exe -U "Power Users" +R SeCreatePermanentPrivilege
+ntrights.exe -U "Power Users" +R SeIncreaseWorkingSetPrivilege
+ntrights.exe -U "Power Users" +R SeIncreaseBasePriorityPrivilege
+rem ntrights.exe -U "Power Users" +R SeLoadDriverPrivilege
+ntrights.exe -U "Power Users" +R SeSystemEnvironmentPrivilege
+ntrights.exe -U "Power Users" +R SeManageVolumePrivilege
+ntrights.exe -U "Power Users" +R SeProfileSingleProcessPrivilege
+ntrights.exe -U "Power Users" +R SeSystemProfilePrivilege
+ntrights.exe -U "Power Users" +R SeShutdownPrivilege
